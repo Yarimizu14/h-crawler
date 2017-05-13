979f80954a2ae6b0390cd595d5f083a474d756d3
Kensei Yarimizu <yarimizu.kensei14@gmail.com>
2017-05-13 15:29:52 +0900

    crawl and save html

--- azure.rb
+++ azure.rb
@@ -20,9 +20,6 @@ module Crawler
       using_wait_time 5 do
         visit('')
       end
-      # fill_in 'login', with: 'hoge'
-      # fill_in 'passwd', with: 'fuga'
-      # click_button 'サインイン'
       find('.nav .ui-show-modal').click
       within(:css, '.ui-modal-contents-inner') do
         fill_in 'user[email]', with: ENV['EMAIL']
@@ -33,13 +30,36 @@ module Crawler
     end
 
     def ff
-      visit('')
-      find '#wrapper'
-      # find '#favoriteservice'
+      p "Index Project Listing Start"
+      @crawls = []
+      within(:css, '#container #main #project-index .projects-index-list') do
+        index = 1
+        all('article').each do |a|
+          href = a.find('.project-title a')[:href]
+          @crawls.push href if href
+        end
+        p @crawls
+      end
+      p "Index Project Listing End"
+    end
+
+    def crawl
+      index = 1
+      @crawls.each do |p|
+        using_wait_time 5 do
+          visit(p)
+          index = index + 1
+          # save_screenshot "screenshot-#{index}.png"
+          File.open("result/wantedly-#{index}.html", 'w') do |f|
+            f.puts page.html
+          end
+        end
+      end
     end
   end
 end
 
 crawler = Crawler::Azure.new
-# crawler.ff
 crawler.login
+crawler.ff
+crawler.crawl
